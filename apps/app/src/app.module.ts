b99779364fda58b3ff6b2981bea6226a4b934012
Max Kovalenko <peiko.kovalenko.m@gmail.com>
2023-02-20 20:28:42 +0200

    Translations

--- apps/app/src/app.module.ts
+++ apps/app/src/app.module.ts
@@ -1,10 +1,21 @@
 import { CoreModule } from '@app/core/core.module';
 import { Module } from '@nestjs/common';
+import { AcceptLanguageResolver, I18nModule } from 'nestjs-i18n';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as path from 'path';
 
 @Module({
-	imports: [CoreModule.forRoot()],
+	imports: [
+		CoreModule.forRoot(),
+		I18nModule.forRoot({
+			resolvers: [AcceptLanguageResolver],
+			fallbackLanguage: 'en',
+			loaderOptions: {
+				path: path.join(__dirname, 'i18n'),
+			},
+		}),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
