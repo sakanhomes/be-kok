b4d0da1a666861877242b9800a976a8e7d3cb3d7
Max Kovalenko <peiko.kovalenko.m@gmail.com>
2023-02-20 21:09:36 +0200

    Migrations system

---
+++ database/knex.ts
@@ -0,0 +1,28 @@
+import * as path from 'path';
+import { config } from 'dotenv';
+
+config({
+    path: path.join(process.cwd(), '.env'),
+    override: true,
+});
+
+const settings = {
+    client: 'mysql2',
+    connection: {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+    },
+    migrations: {
+        tableName: 'migrations',
+        path: 'database/migrations',
+    }
+};
+
+module.exports = {
+    development: settings,
+    production: settings,
+    test: settings,
+};
