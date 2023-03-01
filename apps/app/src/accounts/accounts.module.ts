615777332f5c46c8ae925ed07a0329a0600a00cf
Alex Maltsev <alex_maltsev_b@proton.me>
2023-03-01 10:55:48 +0200

    Account balance updating actions

--- apps/app/src/accounts/accounts.module.ts
+++ apps/app/src/accounts/accounts.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountTransaction } from './models/account-transaction.model';
 import { Account } from './models/account.model';
+import { IncreaseBalanceAction } from './actions/increase-balance.action';
+import { DecreaseBalanceAction } from './actions/decrease-balance.action';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Account, AccountTransaction])],
+    providers: [IncreaseBalanceAction, DecreaseBalanceAction],
 })
 export class AccountsModule {}
---
+++ apps/app/src/accounts/actions/decrease-balance.action.ts
@@ -0,0 +1,27 @@
+import { sleep } from '@app/core/helpers';
+import { ModelLocker } from '@app/core/orm/model-locker';
+import { Injectable } from '@nestjs/common';
+import Decimal from 'decimal.js';
+import { DataSource } from 'typeorm';
+import { Account } from '../models/account.model';
+
+@Injectable()
+export class DecreaseBalanceAction {
+    private locker: ModelLocker;
+
+    public constructor(datasource: DataSource) {
+        this.locker = new ModelLocker(datasource.manager);
+    }
+
+    public async run(account: Account, amount: Decimal.Value): Promise<Account> {
+        const updatedAccount = this.locker.lock(account, async (manager, account) => {
+            account.balance = account.balance.sub(amount);
+
+            await manager.save(account);
+        });
+
+        Object.assign(account, updatedAccount);
+
+        return account;
+    }
+}
