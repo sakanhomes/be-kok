0fd0514f2dc04a637098bdf3e53fa4235d1d39d9
Alex Maltsev <alex_maltsev_b@proton.me>
2023-03-03 10:42:22 +0200

    User settings model

--- apps/app/src/users/users.module.ts
+++ apps/app/src/users/users.module.ts
@@ -4,12 +4,13 @@ import { Account } from '../accounts/models/account.model';
 import { Video } from '../videos/models/video.model';
 import { GetUserVideos } from './actions/get-user-videos.action';
 import { UpdateUserAction } from './actions/update-user.action';
+import { UserSetting } from './models/user-setting.model';
 import { User } from './models/user.model';
 import { ProfileController } from './profile.controller';
 import { UsersController } from './users.controller';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User, Account, Video])],
+    imports: [TypeOrmModule.forFeature([User, UserSetting, Account, Video])],
     providers: [UpdateUserAction, GetUserVideos],
     controllers: [ProfileController, UsersController],
 })
