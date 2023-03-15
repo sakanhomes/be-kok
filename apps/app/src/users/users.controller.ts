b618ea8f9c9d38cde7d414bcb5a2e8d4dd0cd509
Alex Maltsev <alex_maltsev_b@proton.me>
2023-03-15 20:17:53 +0200

    Use model resolving pipe in users controller

--- apps/app/src/users/users.controller.ts
+++ apps/app/src/users/users.controller.ts
@@ -1,10 +1,8 @@
 import { CurrentUser } from '@app/core/auth/decorators/current-user.decorator';
 import { OptionalJwtAuth } from '@app/core/auth/decorators/optional-jwt-auth.decorator';
 import { onlyKeys } from '@app/core/helpers';
-import { ParseAddressPipe } from '@app/core/validation/pipes/parse-address.pipe';
+import { ResolveModelPipe } from '@app/core/orm/pipes/resolve-model.pipe';
 import { Controller, Get, Param } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { VideoResource } from '../videos/resources/video.resource';
 import { GetUserVideos } from './actions/get-user-videos.action';
 import { User } from './models/user.model';
@@ -12,15 +10,11 @@ import { User } from './models/user.model';
 @Controller('users')
 export class UsersController {
     public constructor(
-        @InjectRepository(User)
-        private readonly users: Repository<User>,
         private readonly videosLoader: GetUserVideos,
     ) {}
 
     @Get('/:address')
-    public async entity(@Param('address', ParseAddressPipe) address: string) {
-        const user = await this.users.findOneByOrFail({ address });
-
+    public async entity(@Param('address', ResolveModelPipe) user: User) {
         return {
             user: this.userResponse(user),
         };
@@ -28,8 +22,10 @@ export class UsersController {
 
     @Get('/:address/videos')
     @OptionalJwtAuth()
-    public async videos(@CurrentUser() currentUser: User | null, @Param('address', ParseAddressPipe) address: string) {
-        const user = await this.users.findOneByOrFail({ address });
+    public async videos(
+        @CurrentUser() currentUser: User | null,
+        @Param('address', ResolveModelPipe) user: User,
+    ) {
         const videos = await this.videosLoader.run(user, currentUser?.id !== user.id);
 
         return VideoResource.collection(videos);
