4a397ee44350e6de5b96fff4f73bb4e656d444a3
Alex Maltsev <alex_maltsev_b@proton.me>
2023-03-16 15:08:42 +0200

    Fix subscriptions migration

--- database/migrations/20230315174455_CreateSubscriptionsTable.ts
+++ database/migrations/20230315174455_CreateSubscriptionsTable.ts
@@ -7,9 +7,19 @@ export async function up(knex: Knex): Promise<void> {
         table.bigInteger('subscriberId').unsigned().notNullable();
         table.timestamp('subscribedAt').notNullable();
     });
+
+    await knex.schema.alterTable('users', table => {
+        table.renameColumn('followersAmount', 'subscribersAmount');
+        table.renameColumn('followingsAmount', 'subscriptionsAmount');
+    });
 }
 
 export async function down(knex: Knex): Promise<void> {
     await knex.schema.dropTable('subscriptions');
+
+    await knex.schema.alterTable('users', table => {
+        table.renameColumn('subscribersAmount', 'followersAmount');
+        table.renameColumn('subscriptionsAmount', 'followingsAmount');
+    });
 }
 
