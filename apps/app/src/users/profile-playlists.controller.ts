f37666c721410a09de3efd5fa5456f66978162af
Max Kovalenko <peiko.kovalenko.m@gmail.com>
2023-03-20 18:24:43 +0200

    Filter playlist videos

--- apps/app/src/users/profile-playlists.controller.ts
+++ apps/app/src/users/profile-playlists.controller.ts
@@ -2,7 +2,7 @@ import { CurrentUser } from '@app/core/auth/decorators/current-user.decorator';
 import { JwtAuth } from '@app/core/auth/decorators/jwt-auth.decorator';
 import { OwnershipVerifier } from '@app/core/orm/ownership-verifier';
 import { ResolveModelPipe, ResolveModelUsing } from '@app/core/orm/pipes/resolve-model.pipe';
-import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Query, UsePipes } from '@nestjs/common';
 import { AddVideoToPlaylistAction } from '../playlists/actions/add-video-to-playlist.action';
 import { GetUserPlaylistsAction } from '../playlists/actions/get-user-playlists.action';
 import { LoadPlaylistVideosAction } from '../playlists/actions/load-playlist-videos.actions';
@@ -10,7 +10,9 @@ import { RemoveVideoFromPlaylistAction } from '../playlists/actions/remove-video
 import { Playlist } from '../playlists/models/playlist.model';
 import { PlaylistResource } from '../playlists/resources/playlist.resource';
 import { Video } from '../videos/models/video.model';
+import { FiltersDto } from './dtos/filters.dto';
 import { User } from './models/user.model';
+import { FiltersValidator } from './validators/filters.validator';
 
 @Controller('/me/playlists')
 @JwtAuth()
@@ -30,13 +32,15 @@ export class ProfilePlaylistsController {
     }
 
     @Get('/:publicId')
+    @UsePipes(FiltersValidator)
     public async entity(
         @CurrentUser() user: User,
         @Param('publicId', ResolveModelPipe) playlist: Playlist,
+        @Query() filters: FiltersDto,
     ) {
         OwnershipVerifier.verifyOrFail(user, playlist);
 
-        await this.videosLoader.run(playlist);
+        await this.videosLoader.run(playlist, filters);
 
         return new PlaylistResource(playlist);
     }
