8916669b5e9c5ea266b8566e04b2c300633abff0
Max Kovalenko <peiko.kovalenko.m@gmail.com>
2023-03-21 14:25:39 +0200

    Universal search helper

--- apps/app/src/users/helpers/search.helper.ts
+++ apps/app/src/users/helpers/search.helper.ts
@@ -2,14 +2,18 @@ import { escapeLike } from '@app/core/helpers';
 import { Brackets, SelectQueryBuilder } from 'typeorm';
 import { FiltersDto } from '../dtos/filters.dto';
 
-export class SubsHelper {
-    public static applyFilters<T = any>(query: SelectQueryBuilder<T>, filters: FiltersDto): SelectQueryBuilder<T> {
+export class SearchHelper {
+    public static applyUserSearchFilters<T = any>(
+        query: SelectQueryBuilder<T>,
+        filters: FiltersDto,
+        alias = 'user',
+    ): SelectQueryBuilder<T> {
         if (filters.search && filters.search.length) {
             const search = '%' + escapeLike(filters.search.toLowerCase()) + '%';
 
             query.andWhere(new Brackets(query => {
-                query.orWhere('user.address like :search', { search })
-                    .orWhere('lower(user.name) like :search', { search });
+                query.orWhere(`${alias}.address like :search`, { search })
+                    .orWhere(`lower(${alias}.name) like :search`, { search });
             }));
         }
 
