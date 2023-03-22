d394a2ebae6a016bf30190898c667a73d94ccfda
Alex Maltsev <alex_maltsev_b@proton.me>
2023-03-22 13:33:59 +0200

    Add comment docs

--- apps/app/src/comments/actions/create-comment.action.ts
+++ apps/app/src/comments/actions/create-comment.action.ts
@@ -1,4 +1,5 @@
 import { randomString } from '@app/core/helpers';
+import { ModelLocker } from '@app/core/orm/model-locker';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import Decimal from 'decimal.js';
@@ -10,12 +11,14 @@ import { Comment } from '../models/comment.model';
 @Injectable()
 export class CreateCommentAction {
     public constructor(
+        @InjectRepository(Video)
+        private readonly videos: Repository<Video>,
         @InjectRepository(Comment)
         private readonly comments: Repository<Comment>,
     ) {}
 
-    public run(user: User, video: Video, content: string): Promise<Comment> {
-        const comment = this.comments.create({
+    public async run(user: User, video: Video, content: string): Promise<Comment> {
+        let comment = this.comments.create({
             publicId: randomString(16),
             videoId: video.id,
             userId: user.id,
@@ -25,6 +28,14 @@ export class CreateCommentAction {
             repliesAmount: new Decimal(0),
         });
 
-        return this.comments.save(comment);
+        await ModelLocker.using(this.videos.manager).lock(video, async (manager, video) => {
+            comment = await manager.save(comment);
+
+            video.commentsAmount++;
+
+            await manager.save(video);
+        });
+
+        return comment;
     }
 }
