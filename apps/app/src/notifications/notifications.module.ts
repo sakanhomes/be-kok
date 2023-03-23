6492a5298fe6d2984871a90c93a8928c7ff0a636
Alex Maltsev <alex_maltsev_b@proton.me>
2023-03-23 13:40:43 +0200

    Notifications registration

--- apps/app/src/notifications/notifications.module.ts
+++ apps/app/src/notifications/notifications.module.ts
@@ -1,6 +1,8 @@
+import { Class } from '@app/core/types/class.type';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotifyUserAction } from './actions/notify-user.action';
+import { BaseNotification } from './base.notification';
 import { Notification } from './models/notification.model';
 
 @Module({
@@ -12,4 +14,34 @@ import { Notification } from './models/notification.model';
         NotifyUserAction,
     ],
 })
-export class NotificationsModule {}
+export class NotificationsModule {
+    private static notifications: Map<string, Class<BaseNotification>> = new Map();
+
+    public static registerNotifications(notifications: Class<BaseNotification>[]): void {
+        for (const notification of notifications) {
+            const type: string = (notification as any).type;
+
+            if (!type) {
+                throw new Error(`Type of notification [${notification}] is not specified`);
+            } else if (this.notifications.has(type)) {
+                throw new Error(`Notification type [${type}] is already registered`);
+            }
+
+            this.notifications.set(type, notification);
+        }
+    }
+
+    public static getNotification(type: string): Class<BaseNotification> | null {
+        return this.notifications.get(type) ?? null;
+    }
+
+    public static getNotificationOrFail(type: string): Class<BaseNotification> {
+        const notification = this.getNotification(type);
+
+        if (notification) {
+            return notification;
+        }
+
+        throw new Error(`Notification class for type [${type}] is not registered`);
+    }
+}
