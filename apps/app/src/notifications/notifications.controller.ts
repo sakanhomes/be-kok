cd24cf394a6fbce38a344f91c34279cd3f237c68
Alex Maltsev <alex_maltsev_b@proton.me>
2023-03-24 11:56:00 +0200

    Read notifications

--- apps/app/src/notifications/notifications.controller.ts
+++ apps/app/src/notifications/notifications.controller.ts
@@ -1,10 +1,11 @@
 import { CurrentUser } from '@app/core/auth/decorators/current-user.decorator';
 import { JwtAuth } from '@app/core/auth/decorators/jwt-auth.decorator';
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes } from '@nestjs/common';
 import { User } from '../users/models/user.model';
 import { GetUserNotifications } from './actions/get-user-notifications.action';
 import { MarkNotificationsAsReadAction } from './actions/mark-notifications-as-read.action';
 import { NotificationResource } from './resources/notification.resource';
+import { ReadNotificationsValidator } from './validators/read-notifications.validator';
 
 @Controller('/notifications')
 @JwtAuth()
@@ -19,5 +20,14 @@ export class NotificationsController {
         const notifications = await this.notificationsGetter.run(user);
 
         return NotificationResource.collection(notifications);
+    }
+
+    @Post('/read')
+    @UsePipes(ReadNotificationsValidator)
+    public async read(
+        @CurrentUser() user: User,
+        @Body() { notifications }: {notifications: string[]},
+    ) {
+        await this.reader.run(user, notifications);
     }
 }
