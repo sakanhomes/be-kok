06368099965fef7f7c2e10a51fb0a16584943b85
Alex Maltsev <alex_maltsev_b@proton.me>
2023-05-05 19:00:52 +0300

    Handle null user in ownership verifier

--- libs/core/src/orm/ownership-verifier.ts
+++ libs/core/src/orm/ownership-verifier.ts
@@ -3,13 +3,13 @@ import { ObjectLiteral } from 'typeorm';
 import { ForbiddenException } from '../exceptions/app/forbidden.exception';
 
 export class OwnershipVerifier {
-    public static verifyOrFail(user: User, model: ObjectLiteral, key = 'userId'): void {
+    public static verifyOrFail(user: User | null, model: ObjectLiteral, key = 'userId'): void {
         if (!OwnershipVerifier.verify(user, model, key)) {
             throw new ForbiddenException();
         }
     }
 
-    public static verify(user: User, model: ObjectLiteral, key = 'userId'): boolean {
-        return user.id === model[key];
+    public static verify(user: User | null, model: ObjectLiteral, key = 'userId'): boolean {
+        return user && user.id === model[key];
     }
 }
